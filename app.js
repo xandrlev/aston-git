77d258f1fb50d521655d6749a94f49b1ef976aa1
xandrlev <xandrlev@gmail.com>
2024-02-07 17:13:25 +0300

    docs: answers to questions

--- app.js
+++
@@ -1,7 +0,0 @@
-function checkPalindrom(str) {
-  let newStr = str.replace(/[^a-zA-Z0-9]+/gi, '').toLowerCase();
-  return newStr.toLowerCase() == newStr.toLowerCase().split("").reverse("").join("")
-    ? `${str} is palindrom`
-    : `${str} is not polindrom`;
-}
-console.log(checkPalindrom("Go hang a salami, I'm a lasagna hog"));
---
+++ homeWork_02.js
@@ -0,0 +1,148 @@
+// Задание 1 – Создать объект counter всеми возможными способами;
+
+const counter1 = {};
+const counter2 = new Object();
+const counter3 = Object.create(null);
+const counter4 = {
+  key1: "value1",
+  key2: "value2",
+  innerObj: {
+    key1: "value1",
+    key2: "value2",
+  },
+};
+
+function createMyObject() {
+  return {
+    key1: "value1",
+    key2: "value2",
+  };
+}
+const counter5 = new createMyObject();
+
+class CreateObject {
+  constructor(key1, key2) {
+    this.key1 = key1;
+    this.key2 = key2;
+  }
+}
+const counter6 = new CreateObject("value1", "value2");
+
+const counter7 = Object.fromEntries([
+  ["key1", "value1"],
+  ["key2", "value2"],
+]);
+
+// Задание 2 – Скопировать объект counter всеми возможными способами;
+
+const copyCounter0 = counter7; // условное копирование, по факту создаётся ссылка на другой объект
+const copyCounter1 = Object.assign({}, counter7);
+const copyCounter2 = { ...counter6 };
+const copyCounter3 = JSON.parse(JSON.stringify(counter6));
+const copyCounter4 = structuredClone(counter4);
+// так же можно при помощи функций сторонних бибилиотек (lodash, ramba, etc.)
+
+// Задание 3 – Создать функцию makeCounter всеми описанными и возможными способами;
+
+function makeCounter() {
+  let counter = 0;
+  return function () {
+    return counter++;
+  };
+}
+
+const makeCounter1 = function (count) {
+  return count++;
+};
+
+const makeCounter2 = (count) => ++count;
+
+(function makeCounter3() {
+  return "IIFE";
+})();
+
+const makeCounter4 = new Function("arg1", "arg2", "return arg1 + arg2;");
+
+const obj = {
+  count: 0,
+  makeCounter: function () {
+    return ++this.count;
+  },
+};
+obj.makeCounter();
+
+class Counter {
+  constructor(count) {
+    this.count = count;
+  }
+  makeCounter() {
+    return (this.count += 1);
+  }
+}
+const makeCounter5 = new Counter(5);
+
+const makeCounter6 = async () => {};
+function* myGenerator7() {}
+
+// Бонус Задание 1 – Написать функцию глубокого сравнения двух объектов:
+
+const obj1 = { here: { is: "on", other: "3" }, object: "Y" };
+const obj2 = { here: { is: "on", other: "2" }, object: "Y" };
+
+const deepEqual = (obj1, obj2) => {
+  if (
+    typeof obj1 !== "object" ||
+    obj1 === null ||
+    typeof obj2 !== "object" ||
+    obj2 === null
+  ) {
+    return obj1 === obj2;
+  }
+
+  let keys1 = Object.keys(obj1);
+  let keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+console.log(deepEqual(obj1, obj2));
+
+// Бонус Задание 2 – Развернуть строку в обратном направлении при помощи методов массивов:
+
+function reverseStr(str) {
+  return str.split("").reverse().join("");
+}
+
+function reverseStr(str) {
+  return [...str].reverse().join("");
+}
+
+function reverseStr(str) {
+  return Array.from(str).reduceRight((acc, cur) => acc + cur, "");
+}
+
+function reverseStr(str) {
+  return str
+    .split("")
+    .map((_, index, arr) => arr[arr.length - 1 - index])
+    .join("");
+}
+
+function reverseStr(str) {
+  const reversedArray = [];
+
+  for (let char of str) {
+    reversedArray.unshift(char);
+  }
+
+  return reversedArray.join("");
+}
